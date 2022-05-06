b271605c7c82adc2e2f789a7d43bad0ee7a74931
Pavan <pavans@geekyants.com>
2022-05-06 17:13:35 +0530

    ✨ Implemented moving location on maponce we search and select a new location

--- src/pages/Home/Sections/Map/index.js
+++ src/pages/Home/Sections/Map/index.js
@@ -1,19 +1,32 @@
-import { Fragment } from "react";
+import { Fragment, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import "leaflet/dist/leaflet.css";
 import "leaflet-area-select";
 import { MapContainer, TileLayer } from "react-leaflet";
 
+const ZOOM = 13;
+
 export function Map({ location }) {
-  console.log(location, "**");
+  const mapRef = useRef();
+
+  // Updating the map on location change
+  useEffect(() => {
+    const { current } = mapRef;
+    if (current)
+      current.flyTo(location, ZOOM, {
+        duration: 2,
+      });
+  }, [location]);
+
   return (
     <Fragment>
       <MapContainer
         center={location}
-        zoom={13}
+        zoom={ZOOM}
         scrollWheelZoom={false}
         style={{ height: "100vh" }}
+        ref={mapRef}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
